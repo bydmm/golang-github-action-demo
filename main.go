50fd154dbfbf51dd2c8bf3abae2ea5b0708fa631
chengka <chengka666@163.com>
2021-05-30 16:31:20 +0800

    加入安全检查

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"github-action-demo/pets"
+	"os"
 )
 
 func main() {
 	// 有人会把mysql的密码写代码里并提交到仓库
-	var password = "f62e5bcda4fae4f82370da0c6f20697b8f8447ef"
+	// 所以应该从环境变量中取
+	var password = os.Getenv("password")
 	// 演示代码
 	fmt.Printf("my password %s", password)
 
