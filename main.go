a5e096378a1564c96a643c25afadca1439177a56
chengka <chengka666@163.com>
2021-05-30 15:38:35 +0800

    让猫叫可测

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-func Cat() {
-	fmt.Print("Miao~~~~~")
+func Cat() string {
+	return "Wang~~~~~"
+	// return "Miao~~~~~"
 }
 
 func main() {
-	Cat()
+	saying := Cat()
+	fmt.Print(saying)
 }
