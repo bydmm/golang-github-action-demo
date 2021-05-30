519a8cbbb2f98bfede1c5df2ca8885ed41df6ec2
chengka <chengka666@163.com>
2021-05-30 16:27:59 +0800

    加入安全检查

--- main.go
+++ main.go
@@ -5,12 +5,9 @@ import (
 	"github-action-demo/pets"
 )
 
-var (
-	// 有人会把mysql的密码写代码里并提交到仓库
-	password = "123456"
-)
-
 func main() {
+	// 有人会把mysql的密码写代码里并提交到仓库
+	var password = "f62e5bcda4fae4f82370da0c6f20697b8f8447ef"
 	// 演示代码
 	fmt.Printf("my password %s", password)
 
